b2e67033f0511f6f10c73ca5387bbd8d34cb4f23
Yuki Kawanishi <joliy.puu.yuuki.youta13251414@gmail.com>
2023-08-23 15:51:58 +0900

    Find problem

--- utils/message_creater.py
+++ utils/message_creater.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.http import JsonResponse
+from django.http import HttpResponse,JsonResponse
 import json          #json形式の読み込み
 import csv          #csvの読み込み
 import requests      #気象庁API読み込みに使用
+
 
 def create_single_text_message(message):
     if message[0] == '1':
@@ -12,15 +12,15 @@ def create_single_text_message(message):
         return message
     elif message[0] == '2':
         return
-    elif message[0] == '3':
-        print('Google Mapから現在地を送信してください。')
-        @handler.add(MessageEvent, message=LocationMessage)
-        def handler_location(event):
-            user_address = event.message.address
-            user_latitube = event.message.latitude
-            user_longittube = event.message.longitude
+    elif message == '観光地を調べる':
+        sightseeing_message = [
+            {
+                "type": "text",
+                "text": "現在地をお調べします！"
+            }
 
-            filed_name = find_near_filed(user_latitube)
+        ]
+
         url = 'https://www.jma.go.jp/bosai/forecast/data/overview_forecast/370000.json'     #気象庁API（天気概要）
         res = requests.get(url)
 
@@ -36,10 +36,6 @@ def create_single_text_message(message):
         #weathercodeで場合分け
         weathercode = ["102", "103", "104", "105", "106", "107", "108", "112", "113", "114", "115", "116", "117", "118", "119", "123", "124", "125", "126", "140", "160", "170", "181", "202", "203", "204", "205", "206", "207", "208", "212", "213", "214", "215", "216", "217", "218", "219", "224", "228", "240", "250", "260", "270", "281", "300", "301", "302", "303", "304", "306", "308", "309", "311", "313", "314", "315", "316", "317", "322", "328", "329", "340", "350", "371", "400", "401", "402", "403", "405", "406", "407", "409", "413", "414", "422", "423", "425", "426", "427", "450"]
         for rain in weathercode:
-            if rain == weathercode:
-                if row == '屋内':
-                    print(row) 
-            else:
-                print(row)
-    elif message[0] == '4':
+            
+    elif message == 'お土産を見つける':
         return
