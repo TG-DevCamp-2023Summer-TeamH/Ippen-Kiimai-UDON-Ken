bfa27bda673f4aeaab04b8cc0c179b7c7677551f
Soma Ando <soma.ando.1227@gmail.com>
2023-08-23 14:21:47 +0900

    Change Get Message

--- line_bot/views.py
+++ line_bot/views.py
@@ -11,9 +11,12 @@ def index(request):
     print(request.method)
     if request.method == 'POST':
         request = json.loads(request.body.decode('utf-8'))
-        data = request['events'][0]
-        message = data['message']
-        reply_token = data['replyToken']
-        line_message = LineMessage(create_single_text_message(message['text']))
-        line_message.reply(reply_token)
+        try:
+            data = request['events'][0]
+            message = data['message']
+            reply_token = data['replyToken']
+            line_message = LineMessage(create_single_text_message(message['text']))
+            line_message.reply(reply_token)
+        except:
+            pass
     return HttpResponse("successful")
