f9815f6b842b343cc549caf2fe280e570751982c
Yuki Kawanishi <joliy.puu.yuuki.youta13251414@gmail.com>
2023-08-23 15:54:17 +0900

    Find problem

--- utils/message_creater.py
+++ utils/message_creater.py
@@ -16,11 +16,53 @@ def create_single_text_message(message):
         sightseeing_message = [
             {
                 "type": "text",
-                "text": "現在地をお調べします！"
+                "text": "現在地を以下のクイックリプライから選択するか送信してください。"
+                "quickReply": {
+                    "items":[
+                        {
+                            "type": "action",
+                            "action": {
+                                "type": "message",
+                                "label": "高松市",
+                                "text": "0",
+                            }
+                        },
+                        {
+                            "type": "action",
+                            "action": {
+                                "type": "message",
+                                "label": "中讃",
+                                "text": "1",
+                            }
+                        },
+                        {
+                            "type": "action",
+                            "action": {
+                                "type": "message",
+                                "label": "西讃",
+                                "text": "2",
+                            }
+                        },
+                        {
+                            "type": "action",
+                            "action": {
+                                "type": "message",
+                                "label": "東讃",
+                                "text": "3",
+                            }
+                        },
+                        {
+                            "type": "action",
+                            "action": {
+                                "type": "message",
+                                "label": "島しょ部",
+                                "text": "4",
+                            }
+                        },
+                    ]
+                }
             }
-
         ]
-
         url = 'https://www.jma.go.jp/bosai/forecast/data/overview_forecast/370000.json'     #気象庁API（天気概要）
         res = requests.get(url)
 
@@ -35,7 +77,10 @@ def create_single_text_message(message):
 
         #weathercodeで場合分け
         weathercode = ["102", "103", "104", "105", "106", "107", "108", "112", "113", "114", "115", "116", "117", "118", "119", "123", "124", "125", "126", "140", "160", "170", "181", "202", "203", "204", "205", "206", "207", "208", "212", "213", "214", "215", "216", "217", "218", "219", "224", "228", "240", "250", "260", "270", "281", "300", "301", "302", "303", "304", "306", "308", "309", "311", "313", "314", "315", "316", "317", "322", "328", "329", "340", "350", "371", "400", "401", "402", "403", "405", "406", "407", "409", "413", "414", "422", "423", "425", "426", "427", "450"]
-        for rain in weathercode:
+        if res in weathercode:
+            print()
+        else:
+            print()
             
     elif message == 'お土産を見つける':
         return
